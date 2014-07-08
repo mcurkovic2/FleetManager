c760858b010d27236b6cda92020d78ab652f8e46
nullundefined <matija.curkovic@gmail.com>
2014-07-08 20:07:58 +0200

    VEhicle service

---
+++ fleetman-resource-vehicle/grails-app/services/hr/fleetman/resources/VehicleService.groovy
@@ -0,0 +1,15 @@
+package hr.fleetman.resources
+
+import grails.transaction.Transactional
+
+@Transactional
+class VehicleService {
+
+    def serviceMethod() {
+
+    }
+	
+	def findVehicles() {
+		
+	}
+}
---
+++ fleetman-resource-vehicle/test/unit/hr/fleetman/resources/VehicleServiceSpec.groovy
@@ -0,0 +1,20 @@
+package hr.fleetman.resources
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(VehicleService)
+class VehicleServiceSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
